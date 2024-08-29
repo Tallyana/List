900649974e8e5380bb7fd8e6d525f5d162372670
Tallyana <iggla67@yandex.ru>
2024-08-29 04:49:41 +0400

    1.Данные

--- list.employees/src/main/java/pro/sky/listemployees/EmployeesController.java
+++ list.employees/src/main/java/pro/sky/listemployees/EmployeesController.java
@@ -1,4 +1,16 @@
 package pro.sky.listemployees;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/employees")
+@RestController
 public class EmployeesController {
+    private final EmployeesService EmployeesService;
+
+    public EmployeesController(EmployeesService employeesService) {
+
+        this.employeesService = employeesService;
+    }
 }
--- list.employees/src/main/java/pro/sky/listemployees/EmployeesService.java
+++ list.employees/src/main/java/pro/sky/listemployees/EmployeesService.java
@@ -1,4 +1,13 @@
 package pro.sky.listemployees;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeesService {
+
+    private  String fullName(String getfistName,String getlastName){
+        return fullName(" a ", " b ");
+
+    }
+
 }
